133c450d6e9cd07d0b466872851b74975264b548
letcla0624 <letcla0624@yahoo.com.tw>
2022-08-18 18:09:36 +0800

    Update 2022-08-18T10:09:33.901Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -7,26 +7,22 @@ if (elem) {
     format: "yyyy/mm/dd",
     language: "zh-TW"
   });
-}
+} // const buttonsUl_li = document.querySelectorAll(".buttonsUl > li");
+// buttonsUl_li.forEach((e) => {
+//   e.addEventListener("click", function () {
+//     buttonsUl_li.forEach((item) => item.classList.remove("active"));
+//     this.classList.add("active");
+//   });
+// });
+// const path_name = window.location.pathname.split("/").pop().slice(0, -5);
+// const index = document.querySelector(".index");
+// const classes = document.querySelector(".classes");
+// if (path_name === "" || path_name === "index") {
+//   index.classList.add("active");
+// } else if (path_name === "classes" || path_name === "class-intro") {
+//   classes.classList.add("active");
+// }
 
-var buttonsUl_li = document.querySelectorAll(".buttonsUl > li");
-buttonsUl_li.forEach(function (e) {
-  e.addEventListener("click", function () {
-    buttonsUl_li.forEach(function (item) {
-      return item.classList.remove("active");
-    });
-    this.classList.add("active");
-  });
-});
-var path_name = window.location.pathname.split("/").pop().slice(0, -5);
-var index = document.querySelector(".index");
-var classes = document.querySelector(".classes");
-
-if (path_name === "" || path_name === "index") {
-  index.classList.add("active");
-} else if (path_name === "classes" || path_name === "class-intro") {
-  classes.classList.add("active");
-}
 
 var mySwiper = document.querySelector(".mySwiper");
 
